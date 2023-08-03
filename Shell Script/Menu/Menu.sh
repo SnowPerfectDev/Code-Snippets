c0b11afda0edbabf31bc2d6fbc1f04cce8af7384
Mercenario <83768808+SnowPerfectDev@users.noreply.github.com>
2023-08-03 19:07:17 -0300

    Menu.sh

--- Shell Script/Menu/Menu.sh
+++ Shell Script/Menu/Menu.sh
@@ -1,1 +1,26 @@
-### Simples menu, alternativa ao menu select do bash.
+
+#!/bin/bash
+# Alternativa simples ao ao menu Select.
+
+
+# OPÇÕES que vão aparecer no menu
+MENU=(
+    'Firefox'
+    'Chromium'
+    'Leafpad'
+    'Gedit'
+)
+
+# Iniciando
+while true; do
+   i=0 # volte para 0 :)
+   # Imprima o menu
+   for menu in "${MENU[@]}"; do
+      echo "[$i]  ${menu}"
+      let i++
+   done
+   # Leia a opção ;)
+   read -ep $'\n> ' menu_choice
+   # USE O CASE/IF aqui.
+   printf "Você selecionou: ${MENU[$menu_choice]}\n\n"
+done
